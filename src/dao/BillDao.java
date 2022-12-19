e38a3d05e52a1b684eefac461bd329346aa88de5
Rishab Dugar <56007479+kingrishabdugar@users.noreply.github.com>
2022-12-19 19:14:06 +0530

    Add files via upload

--- src/dao/BillDao.java
+++ src/dao/BillDao.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -87,3 +88,94 @@ public class BillDao {
         
     }
 }
+=======
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import javax.swing.JOptionPane;
+import java.sql.*;
+import java.util.ArrayList;
+import model.Bill;
+
+/**
+ *
+ * @author kingrishabdugar
+ */
+public class BillDao {
+
+    public static String getId() {
+        int id = 1;
+        try {
+            ResultSet rs = DbOperations.getData("select max(id) from bill");
+            if (rs.next()) {
+                id = rs.getInt(1);
+                id = id + 1;
+            }
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+        }
+        return String.valueOf(id);
+    }
+    
+    public static void save(Bill bill){
+        String query = "insert into bill values('"+bill.getId()+"','"+bill.getName()+"','"+bill.getMobileNumber()+"','"+bill.getEmail()+"','"+bill.getDate()+"','"+bill.getTotal()+"','"+bill.getCreatedBy()+"')";
+        DbOperations.setDataorDelete(query, "Bill Details Added Successfully");
+        
+    }
+    
+    public static ArrayList<Bill> getAllRecordsByInc(String date){
+        ArrayList<Bill> arrayList = new ArrayList<>();
+        try{
+            ResultSet rs = DbOperations.getData("Select * from bill where date like '%"+date+"%'");
+            while(rs.next()){
+                Bill bill = new Bill();
+                bill.setId(rs.getInt("id"));
+                bill.setName(rs.getString("name"));
+                bill.setMobileNumber(rs.getString("mobileNumber"));
+                bill.setEmail(rs.getString("email"));
+                bill.setDate(rs.getString("date"));
+                bill.setTotal(rs.getString("total"));
+                bill.setCreatedBy(rs.getString("createdBy"));
+                arrayList.add(bill);
+                
+            }
+        }
+        catch(Exception e){
+            JOptionPane.showMessageDialog(null,e);
+        }
+        return arrayList;
+    }
+    
+    public static ArrayList<Bill> getAllRecordsByDesc(String date){
+        ArrayList<Bill> arrayList = new ArrayList<>();
+        try{
+            ResultSet rs = DbOperations.getData("Select * from bill where date like '%"+date+"%' order by id DESC");//"DESC" sql query arrange automatically in sorted order
+            while(rs.next()){
+                Bill bill = new Bill();
+                bill.setId(rs.getInt("id"));
+                bill.setName(rs.getString("name"));
+                bill.setMobileNumber(rs.getString("mobileNumber"));
+                bill.setEmail(rs.getString("email"));
+                bill.setDate(rs.getString("date"));
+                bill.setTotal(rs.getString("total"));
+                bill.setCreatedBy(rs.getString("createdBy"));
+                arrayList.add(bill);
+                
+            }
+        }
+        catch(Exception e){
+            JOptionPane.showMessageDialog(null,e);
+        }
+        return arrayList;
+    }
+    public static void delete(String id){
+        String query = "delete from bill where id ='"+id+"' "; // delete from table bill
+        DbOperations.setDataorDelete(query, "Invoice/Order Refunded Successfully !");
+        
+    }
+}
+>>>>>>> fe35325a78edd5e7ac8be8dbe354e80dedc53409
--- src/dao/ConnectionProvider.java
+++ src/dao/ConnectionProvider.java
@@ -1,3 +1,59 @@
+<<<<<<< HEAD
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+import java.sql.*;
+/**
+ *
+ * @author kingrishabdugar
+ */
+public class ConnectionProvider {
+    
+    static String username;
+    static String password;
+    static String server;
+    static int id = 1;
+    public static void getid(int a)
+    {
+        id=a;
+    }
+    public static void setusername(String id) {
+        ConnectionProvider.username = id;
+    }
+    public static void setpassword(String password) {
+        ConnectionProvider.password = password;
+    }
+    public static void setserver(String server) {
+        ConnectionProvider.server = server;
+    }
+    public static Connection getCon()
+    {
+        try
+        {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            //root and "Password" for MySQL login in MySQL login locally in Client
+            //Local Server
+            if(id == 0)
+            {
+                Connection con = DriverManager.getConnection("jdbc:mysql://localhost:"+server+"/cms?useSSL=false",username,password);
+                return con;
+            }
+            else
+            {
+            //Online SQL Server
+            Connection con = DriverManager.getConnection("jdbc:mysql://db4free.net:3306/greenleafgourmet?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&useSSL=false","kingrishabdugar","i3Enq7@gM!maYmn");
+            // Win + R  -> services.msc -> MySQL (manual) -> Start -> MySql client -> Login with password
+            return con;
+            }
+        }
+        catch(Exception e){
+            return null;
+        }   
+    }
+}
+=======
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -28,3 +84,4 @@ public class ConnectionProvider {
         }   
     }
 }
+>>>>>>> fe35325a78edd5e7ac8be8dbe354e80dedc53409

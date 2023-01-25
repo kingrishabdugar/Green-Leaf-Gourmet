233fa5636c347fe32e86940b04e9c4d6e2d408bf
Rishab Dugar <56007479+kingrishabdugar@users.noreply.github.com>
2023-01-25 14:13:55 +0530

    JPanel Encryption etc.

---
+++ Green-Leaf-Gourmet_src/common/Main.java
@@ -0,0 +1,73 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package common;
+
+/**
+ *
+ * @author kingr
+ */
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Main {
+    public static void main(String[] args) {
+        new Main();
+    }
+
+    public Main() {
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    JFrame frame = new JFrame();
+                    frame.add(new BackgroundPane());
+                    frame.pack();
+                    frame.setLocationRelativeTo(null);
+                    frame.setVisible(true);
+                } catch (IOException ex) {
+                    Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        });
+    }
+    
+    public class BackgroundPane extends JPanel {
+
+        private BufferedImage backgroundImage;
+
+        public BackgroundPane() throws IOException {
+            backgroundImage = ImageIO.read(getClass().getResource("/images/largesignup.png"));
+        }
+
+        @Override
+        public Dimension getPreferredSize() {
+            if (backgroundImage == null) {
+                return new Dimension(400, 400);
+            }
+            return new Dimension(backgroundImage.getWidth(), backgroundImage.getHeight());
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            if (backgroundImage == null) {
+                return;
+            }
+            Graphics2D g2d = (Graphics2D) g.create();
+            g2d.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
+            g2d.dispose();
+        }
+
+    }
+}
